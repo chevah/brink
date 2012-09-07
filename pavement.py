148d4c723a0bb6f65444180805f7272ec416a68d
Adi Roiban <adi.roiban@chevah.com>
2012-09-07 12:47:05 +0300

    Update pypi tasks.

--- pavement.py
+++ pavement.py
@@ -13,7 +13,7 @@ from pavement_lib import (
     default,
     pave,
     )
-from paver.easy import task
+from paver.easy import consume_args, task
 
 # Make pylint shut up.
 help
@@ -27,16 +27,70 @@ def deps():
 
 
 @task
-def pypi_mirror():
+@consume_args
+def pypi_mirror(args):
     """
-    Create a mirror of required pypi packages.
+    Create a local mirror of required pypi packages.
     """
     pave.fs.deleteFolder([pave.path.pypi])
     pave.fs.createFolder([pave.path.pypi])
+
+    arguments = [
+            '-d', pave.path.pypi,
+            '--no-install',
+            ]
+
+    if args:
+        arguments.extend(args)
+    else:
+        arguments.extend(['-r', 'requirements.txt'])
+
     pave.pip(
         command='install',
-        arguments=[
+        arguments=arguments,
+        )
+
+
+@task
+@consume_args
+def pypi_download(args):
+    """
+    Download package from central PyPI mirror.
+    """
+    pave.fs.createFolder([pave.path.pypi])
+
+    arguments = [
             '-d', pave.path.pypi,
-            '-r', 'requirements.txt',
-            ],
+            '--no-install',
+            ]
+    if not args:
+        print "You must specify at least one package to be downloaded."
+        return
+    arguments.extend(args)
+
+    pave.pip(
+        command='install',
+        arguments=arguments,
+        only_cache=False,
+        index_url='http://pypi.python.org/simple',
+        )
+
+
+@task
+@consume_args
+def pypi_install(args):
+    """
+    Install the package from local cache.
+    """
+    arguments = ['--upgrade']
+
+    if not args:
+        print "You must specify one package to be installed."
+        return
+
+    arguments.extend(args)
+
+    pave.pip(
+        command='install',
+        arguments=arguments,
         )
