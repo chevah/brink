650960884b27ffa692c9228434dfdaf364ea3f80
Adi Roiban <adi.roiban@chevah.com>
2012-10-10 11:41:31 +0300

    Create package for brink.

--- paver.sh
+++ paver.sh
@@ -55,153 +55,185 @@ write_default_values() {
 }
 
 
-# Set default Python version.
-# It can be overwritten by specific operating system versions.
-PYTHON_BIN="/bin/python"
+update_brink() {
+    raw_version=`grep "BRINK_VERSION =" pavement.py`
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        # Brink version was not found, so we go with default.
+        echo "Installing latest version of pavement_lib.py..."
+        cp ${PAVEMENT_LIB_PATH} pavement_lib.py
+        return
+    fi
 
-PYTHON_VERSION="python2.5"
-PYTHON_LIB="/lib/${PYTHON_VERSION}/"
+    # Extract version and remove quotes.
+    version=${raw_version#BRINK_VERSION = }
+    version=${version#\'}
+    version=${version%\'}
+    version=${version#\"}
+    version=${version%\"}
+    echo "Installing version: chevah-brink==$version of brink..."
 
-ARCH='x86'
+    ${PYTHON_BIN} -m \
+        pip install chevah-brink==$version \
+            --index-url=172.0.0.1:10042/simple \
+            --download-cache=${PROJECT_ROOT}/brink/cache/pypi \
+            --find-links=file://${PROJECT_ROOT}/brink/cache/pypi
 
-OS=`uname -s | tr '[:upper:]' '[:lower:]'`
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        echo "Failed to install brink."
+        exit 1
+    fi
+}
 
-if [ "${OS%mingw*}" = "" ] ; then
 
-    OS='windows'
-    ARCH='x86'
-    PYTHON_BIN="/lib/python.exe"
-    PYTHON_LIB="/lib/Lib/"
+detect_os() {
+    OS=`uname -s | tr '[:upper:]' '[:lower:]'`
 
-elif [ "${OS}" = "sunos" ] ; then
+    if [ "${OS%mingw*}" = "" ] ; then
 
-    OS="solaris"
-    ARCH=`uname -p`	
-    VERSION=`uname -r`
+        OS='windows'
+        ARCH='x86'
+        PYTHON_BIN="/lib/python.exe"
+        PYTHON_LIB="/lib/Lib/"
 
+    elif [ "${OS}" = "sunos" ] ; then
+
+        OS="solaris"
+        ARCH=`uname -p`	
+        VERSION=`uname -r`
+
+        if [ "$ARCH" = "i386" ] ; then
+            ARCH='x86'
+        fi
+
+        if [ "$VERSION" = "5.10" ] ; then
+            OS="solaris10"
+        fi
+
+    elif [ "${OS}" = "aix" ] ; then
+
+        release=`oslevel`
+        case $release in
+            5.1.*)
+                OS='aix51'
+                ARCH='ppc'
+                ;;
+            *)
+                # By default we go for AIX 5.3 on PPC64
+                OS='aix53'
+                ARCH='ppc64'
+            ;;
+        esac
+
+    elif [ "${OS}" = "hp-ux" ] ; then
+
+        OS="hpux"
+        ARCH=`uname -m`
+
+    elif [ "${OS}" = "linux" ] ; then
+
+        ARCH=`uname -m`
+
+        if [ -f /etc/redhat-release ] ; then
+            # Careful with the indentation here.
+            # Make sure rhel_version does not has spaces before and after the
+            # number.
+            rhel_version=`\
+                cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
+            # RHEL4 glibc is not compatible with RHEL 5 and 6.
+            rhel_major_version=${rhel_version%.*}
+            if [ "$rhel_major_version" = "4" ] ; then
+                OS='rhel4'
+            elif [ "$rhel_major_version" = "5" ] ; then
+                OS='rhel5'
+            elif [ "$rhel_major_version" = "6" ] ; then
+                OS='rhel6'
+            else
+                echo 'Unsuported RHEL version.'
+                exit 1
+            fi
+        elif [ -f /etc/SuSE-release ] ; then
+            sles_version=`\
+                grep VERSION /etc/SuSE-release | sed s/VERSION\ =\ //`
+            if [ "$sles_version" = "11" ] ; then
+                OS='sles11'
+            else
+                echo 'Unsuported SLES version.'
+                exit 1
+            fi
+        elif [ -f /etc/lsb-release ] ; then
+            release=`lsb_release -sr`
+            case $release in
+                '10.04' | '10.10' | '11.04' | '11.10')
+                    OS='ubuntu1004'
+                ;;
+                '12.04' | '12.10' | '13.04' | '13.10')
+                    OS='ubuntu1204'
+                    PYTHON_VERSION="python2.5"
+                    PYTHON_LIB="/lib/${PYTHON_VERSION}/"
+                ;;
+                *)
+                    echo 'Unsuported Ubuntu version.'
+                    exit 1
+                ;;
+            esac
+    	    
+        elif [ -f /etc/slackware-version ] ; then
+
+            # For Slackware, for now we use Ubuntu 10.04.
+            # Special dedication for all die hard hackers like Ion.
+    	    OS="ubuntu1004"
+
+        elif [ -f /etc/debian_version ] ; then
+            OS="debian"
+
+        fi
+
+    elif [ "${OS}" = "darwin" ] ; then
+        osx_version=`sw_vers -productVersion`
+        osx_major_version=${osx_version%.*}
+    	if [ "$osx_major_version" = "10.4" ] ; then
+    		OS='osx104'
+    	else
+    		echo 'Unsuported OS X version.'
+    		exit 1
+    	fi
+    	
+    	osx_arch=`uname -m`
+    	if [ "$osx_arch" = "Power Macintosh" ] ; then
+    		ARCH='ppc'
+    	else
+    		echo 'Unsuported OS X architecture.'
+    		exit 1
+    	fi
+    else
+        echo 'Unsuported operating system.'
+        exit 1
+    fi
+
+    # Fix arch names.
+    if [ "$ARCH" = "i686" ] ; then
+        ARCH='x86'
+
+    fi
     if [ "$ARCH" = "i386" ] ; then
         ARCH='x86'
     fi
 
-    if [ "$VERSION" = "5.10" ] ; then
-        OS="solaris10"
+    if [ "$ARCH" = "x86_64" ] ; then
+        ARCH='x64'
     fi
+}
 
-elif [ "${OS}" = "aix" ] ; then
+# Put default values and create them as global variables.
+OS='not-detected-yet'
+ARCH='x86'
+PYTHON_BIN="/bin/python"
+PYTHON_VERSION="python2.5"
+PYTHON_LIB="/lib/${PYTHON_VERSION}/"
 
-    release=`oslevel`
-    case $release in
-        5.1.*)
-            OS='aix51'
-            ARCH='ppc'
-            ;;
-        *)
-            # By default we go for AIX 5.3 on PPC64
-            OS='aix53'
-            ARCH='ppc64'
-        ;;
-    esac
-
-elif [ "${OS}" = "hp-ux" ] ; then
-
-    OS="hpux"
-    ARCH=`uname -m`
-
-elif [ "${OS}" = "linux" ] ; then
-
-    ARCH=`uname -m`
-
-    if [ -f /etc/redhat-release ] ; then
-        # Careful with the indentation here.
-        # Make sure rhel_version does not has spaces before and after the
-        # number.
-        rhel_version=`\
-            cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
-        # RHEL4 glibc is not compatible with RHEL 5 and 6.
-        rhel_major_version=${rhel_version%.*}
-        if [ "$rhel_major_version" = "4" ] ; then
-            OS='rhel4'
-        elif [ "$rhel_major_version" = "5" ] ; then
-            OS='rhel5'
-        elif [ "$rhel_major_version" = "6" ] ; then
-            OS='rhel6'
-        else
-            echo 'Unsuported RHEL version.'
-            exit 1
-        fi
-    elif [ -f /etc/SuSE-release ] ; then
-        sles_version=`\
-            grep VERSION /etc/SuSE-release | sed s/VERSION\ =\ //`
-        if [ "$sles_version" = "11" ] ; then
-            OS='sles11'
-        else
-            echo 'Unsuported SLES version.'
-            exit 1
-        fi
-    elif [ -f /etc/lsb-release ] ; then
-        release=`lsb_release -sr`
-        case $release in
-            '10.04' | '10.10' | '11.04' | '11.10')
-                OS='ubuntu1004'
-            ;;
-            '12.04' | '12.10' | '13.04' | '13.10')
-                OS='ubuntu1204'
-                PYTHON_VERSION="python2.5"
-                PYTHON_LIB="/lib/${PYTHON_VERSION}/"
-            ;;
-            *)
-                echo 'Unsuported Ubuntu version.'
-                exit 1
-            ;;
-        esac
-	    
-    elif [ -f /etc/slackware-version ] ; then
-
-        # For Slackware, for now we use Ubuntu 10.04.
-        # Special dedication for all die hard hackers like Ion.
-	    OS="ubuntu1004"
-
-    elif [ -f /etc/debian_version ] ; then
-        OS="debian"
-
-    fi
-
-elif [ "${OS}" = "darwin" ] ; then
-    osx_version=`sw_vers -productVersion`
-    osx_major_version=${osx_version%.*}
-	if [ "$osx_major_version" = "10.4" ] ; then
-		OS='osx104'
-	else
-		echo 'Unsuported OS X version.'
-		exit 1
-	fi
-	
-	osx_arch=`uname -m`
-	if [ "$osx_arch" = "Power Macintosh" ] ; then
-		ARCH='ppc'
-	else
-		echo 'Unsuported OS X architecture.'
-		exit 1
-	fi
-else
-    echo 'Unsuported operating system.'
-    exit 1
-fi
-
-
-# Fix arch names.
-if [ "$ARCH" = "i686" ] ; then
-    ARCH='x86'
-
-fi
-if [ "$ARCH" = "i386" ] ; then
-    ARCH='x86'
-fi
-
-if [ "$ARCH" = "x86_64" ] ; then
-    ARCH='x64'
-fi
-
+detect_os
 
 find_project_root
 
@@ -216,12 +248,14 @@ export PYTHONPATH=${PYTHON_FOLDER}
 PYTHON_DISTRIBUTABLE=${BOOTSTRAP_PATH}/cache/${PYTHON_DIST}
 PAVEMENT_LIB_PATH=${BOOTSTRAP_PATH}/pavement_commons.py
 
-
 if [ "$1" = "clean" ] ; then
     clean_build
     exit 0
 fi
 
+if [ "$1" = "deps" ] ; then
+    update_brink
+fi
 
 if [ "$1" = "get_default_values" ] ; then
     write_default_values
@@ -280,15 +314,15 @@ cp ${BOOTSTRAP_PATH}/distribute/easy_install.py ${PYTHON_LIB}/
 # Unix.
 current_root=`pwd`
 
-echo "Update pavement_lib.py."
-cp ${PAVEMENT_LIB_PATH} pavement_lib.py
-
 if [ $WAS_PYTHON_JUST_INSTALLED -eq 1 ]; then
+
+    update_brink
+
     ${PYTHON_BIN} -c 'from paver.tasks import main; main()' deps
     python_exit_code=$?
     if [ $python_exit_code -ne 0 ]; then
         echo 'Failed to run the inital "paver deps" command.'
-        exit python_exit_code
+        exit 1
     fi
 fi
 
---
+++ setup.py
@@ -0,0 +1,55 @@
+"""
+Package for chevah-brink.
+
+These are the extensions build around paver.
+"""
+from distutils import log
+from setuptools import setup, Command
+import os
+import shutil
+
+VERSION = '0.0.2'
+
+
+class CacheCommand(Command):
+    """
+    Copy the sdist files to local pypi cache.
+    """
+
+    description = "copy distributable to Chevah cache folder"
+    user_options = []
+
+    def initialize_options(self):
+        self.cwd = None
+        self.destination_base = '~/chevah/brink/cache/pypi/'
+
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+
+    def run(self):
+        assert os.getcwd() == self.cwd, (
+            'Must be in package root: %s' % self.cwd)
+        self.run_command('sdist')
+        sdist_command = self.distribution.get_command_obj('sdist')
+        for archive in sdist_command.archive_files:
+            source = os.path.join(archive)
+            destination = os.path.expanduser(
+                self.destination_base + os.path.basename(archive))
+            shutil.copyfile(source, destination)
+        log.info(
+            "Distributables files copied to %s " % (self.destination_base))
+
+
+setup(
+    name='chevah-brink',
+    version=VERSION,
+    maintainer="Adi Roiban",
+    maintainer_email="adi.roiban@chevah.com",
+    license='BSD (3 clause) License',
+    platforms='any',
+    description='Sphinx themes used by SFTPPlus products.',
+    long_description=open('README.rst').read(),
+    url='http://www.chevah.com',
+    py_modules=['pavement_commons'],
+    cmdclass={'cache': CacheCommand},
+)
