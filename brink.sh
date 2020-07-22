e456025f27052806d86eaba4708e80f4bc34598e
dumol <dumol@gnome.org>
2020-07-22 11:30:02 +0300

    Update brink.sh from server master.

--- brink.sh
+++ brink.sh
@@ -411,8 +411,8 @@ get_python_dist() {
         # We have the requested python version.
         get_binary_dist $python_distributable $remote_base_url/${OS}/${ARCH}
     else
-        echo "Requested version was not found on the remote server."
-        echo "$remote_base_url $python_distributable"
+        (>&2 echo "Requested version was not found on the remote server.")
+        (>&2 echo "$remote_base_url $python_distributable")
         exit 4
     fi
 }
